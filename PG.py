67e7d16cce289a7d3c0e5ccceb59f7aa4df25b2e
judinus10 <judes.20222403@iit.ac.lk>
2024-08-20 09:40:37 +0530

    completed the main method

--- PG.py
+++ PG.py
@@ -7,4 +7,21 @@ def replaceWithNum():
 def replacewithUpperCase():
     pass
 def main():
-    pass
+    numPassWord = int(input("Enter the No. Of do you want to generate : "))
+
+    print("Generating "+numPassWord+" Passwords.")
+
+    passwordLeangth = []
+
+    print("Minimum length of password should be 4")
+
+    for i in range(numPassWord):
+        length = int(input("Enter the leangth of the Password #"+str(i+1)+" : "))
+        if length<4:
+            length=4
+        passwordLeangth.append(length)
+
+    password = generatePassword(passwordLeangth)
+
+    for i in range (numPassWord):
+        print("Password #"+str(i+1)+" = "+password[i])
